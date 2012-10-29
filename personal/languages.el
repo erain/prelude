34873487bbe40a7c6f1cb03804b4ff72705dacf2
YI YU <YiYu@Ymail.com>
2012-10-29 16:29:27 +0800

    change the pycheckers to the right executable version

--- personal/languages.el
+++ personal/languages.el
@@ -5,7 +5,7 @@
            (local-file (file-relative-name
                         temp-file
                         (file-name-directory buffer-file-name))))
-      (list "pyflakes" (list local-file))))
+      (list "pycheckers" (list local-file))))
   (add-to-list 'flymake-allowed-file-name-masks
                '("\\.py\\'" flymake-pyflakes-init)))
 (add-hook 'python-mode-hook 'flymake-mode)
