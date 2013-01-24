2e3ad5074e3b2b3bfd70a26dd6f5a8c00708d45a
Yu YI <yiyu@ymail.com>
2013-01-24 10:16:00 +0800

    config indentation for haskell mode

--- personal/languages.el
+++ personal/languages.el
@@ -1,3 +1,6 @@
+;;;;
+;; Python Syntax Check
+;;;;
 (when (load "flymake" t)
   (defun flymake-pyflakes-init ()
     (let* ((temp-file (flymake-init-create-temp-buffer-copy
@@ -9,3 +12,10 @@
   (add-to-list 'flymake-allowed-file-name-masks
                '("\\.py\\'" flymake-pyflakes-init)))
 (add-hook 'python-mode-hook 'flymake-mode)
+
+
+;;;;
+;; Haskell Indentation
+;;;;
+(custom-set-variables
+ '(haskell-mode-hook '(turn-on-haskell-indentation)))
