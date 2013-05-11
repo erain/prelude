2cc9f16b8626477d5a5cd9dfa7a9b8aac7c0b691
Yu YI <yiyu@ymail.com>
2013-05-11 20:38:14 +0800

    Add flymake check for python mode

--- personal/languages.el
+++ personal/languages.el
@@ -1,6 +1,43 @@
 ;;;;
-;; Python Syntax Check
+;; Python
+;; http://caisah.info/emacs-for-python/
 ;;;;
+(add-hook 'python-mode-hook 'auto-complete-mode)
+(add-hook 'python-mode-hook 'jedi:setup)
+(add-hook 'python-mode-hook 'flymake)
+
+(setq pycodechecker "pylint_etc_wrapper.py")
+(when (load "flymake" t)
+  (load-library "flymake-cursor")
+  (defun dss/flymake-pycodecheck-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list pycodechecker (list local-file))))
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" dss/flymake-pycodecheck-init)))
+
+(defun dss/pylint-msgid-at-point ()
+  (interactive)
+  (let (msgid
+        (line-no (line-number-at-pos)))
+    (dolist (elem flymake-err-info msgid)
+      (if (eq (car elem) line-no)
+          (let ((err (car (second elem))))
+            (setq msgid (second (split-string (flymake-ler-text err)))))))))
+
+(defun dss/pylint-silence (msgid)
+  "Add a special pylint comment to silence a particular warning."
+  (interactive (list (read-from-minibuffer "msgid: " (dss/pylint-msgid-at-point))))
+  (save-excursion
+    (comment-dwim nil)
+    (if (looking-at "pylint:")
+        (progn (end-of-line)
+               (insert ","))
+      (insert "pylint: disable-msg="))
+    (insert msgid)))
 
 
 ;;;;
