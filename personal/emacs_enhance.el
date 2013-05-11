960799f539fad88e769dd64a96cb75e7d3937762
Yu YI <yiyu@ymail.com>
2013-05-11 20:38:14 +0800

    Add flymake check for python mode

--- personal/emacs_enhance.el
+++ personal/emacs_enhance.el
@@ -102,8 +102,23 @@
                   (line-beginning-position (+ 1 arg)))
   (message "%d line%s copied" arg (if (= 1 arg) "" "s")))
 
+;;;;;;
+;; el-get configurations
+;;;;;;
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
+(el-get 'sync)
 
 
+;;;;;;
+;; package.el repos
+;;;;
 (setq package-archives '(("ELPA" . "http://tromey.com/elpa/")
                          ("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
