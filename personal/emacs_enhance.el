b900e8ca7112cb95075209892bddcce041b8d36d
Yu Yi <yiyu@amazon.com>
2015-06-16 16:38:36 -0700

    heml everywhere

--- personal/emacs_enhance.el
+++ personal/emacs_enhance.el
@@ -166,6 +166,7 @@
 ;;;;;;
 (require 'helm)
 (require 'helm-config)
+(require 'prelude-helm-everywhere)
 
 ;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
 ;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
