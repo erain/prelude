68614a85e3cabc86e102c8ea0e658f5292be191a
Yu Yi <yiyu@google.com>
2019-08-24 18:26:07 -0400

    Make emacs install packages by default

--- personal/preload/use_package.el
+++ personal/preload/use_package.el
@@ -9,3 +9,18 @@
 
 (require 'use-package)
 (setq use-package-always-ensure t)
+
+(defun ensure-package-installed (&rest packages)
+  "Assure every package is installed, ask for installation if it’s not.
+   Return a list of installed packages or nil for every skipped package."
+  (mapcar
+   (lambda (package)
+     ;; (package-installed-p 'evil)
+     (if (package-installed-p package)
+         nil
+       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+           (package-install package)
+         package)))
+   packages))
+
+(ensure-package-installed 'helm-projectile 'neotree 'meghanada 'gruvbox-theme) ;  --> (nil nil) if iedit and magit are already installed
