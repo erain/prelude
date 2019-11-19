f675569cc54c35a9821ac6ed4996e0f63c70c0f9
Yu Yi <yiyu@google.com>
2019-11-19 16:30:37 -0500

    Delete hydra setups for now (as it is not working)

--- personal/lsp.el
+++ personal/lsp.el
@@ -1,9 +1,3 @@
-(use-package hydra
-  :ensure t)
-
-(use-package use-package-hydra
-  :ensure t)
-
 (use-package helm-lsp
   :commands helm-lsp-workspace-symbol
   :config
@@ -18,9 +12,6 @@
       (call-interactively #'helm-lsp-global-workspace-symbol))))
 
 
-(use-package lsp-ivy
-  :ensure t)
-
 
 (use-package yasnippet                  ; Snippets
   :ensure t
@@ -30,86 +21,22 @@
   (yas-global-mode t))
 
 
+
 (use-package ccls
   :hook ((c-mode c++-mode objc-mode cuda-mode) .
          (lambda () (require 'ccls) (lsp))))
 
 
+
 (use-package lsp-mode
   :commands lsp
-  :requires hydra helm helm-lsp
+  :requires helm helm-lsp
   :hook ((c++-mode . lsp)
          (go-mode . lsp))
-  :after hydra
-  :bind (:map lsp-mode-map ("C-c C-l" . hydra-lsp/body))
-  :hydra (defhydra hydra-lsp (:exit t :hint nil)
-           "
- Buffer^^               Server^^                   Symbol
--------------------------------------------------------------------------------------
- [_f_] format           [_M-r_] restart            [_d_] declaration  [_i_] implementation  [_o_] documentation
- [_m_] imenu            [_S_]   shutdown           [_D_] definition   [_t_] type            [_r_] rename
- [_x_] execute action   [_M-s_] describe session   [_R_] references   [_s_] signature"
-           ("d" lsp-find-declaration)
-           ("D" lsp-ui-peek-find-definitions)
-           ("R" lsp-ui-peek-find-references)
-           ("i" lsp-ui-peek-find-implementation)
-           ("t" lsp-find-type-definition)
-           ("s" lsp-signature-help)
-           ("o" lsp-describe-thing-at-point)
-           ("r" lsp-rename)
-
-           ("f" lsp-format-buffer)
-           ("m" lsp-ui-imenu)
-           ("x" lsp-execute-code-action)
-
-           ("M-s" lsp-describe-session)
-           ("M-r" lsp-restart-workspace)
-           ("S" lsp-shutdown-workspace))
-
   :config
 
   (setq lsp-prefer-flymake nil)  ;; Prefer using lsp-ui (flycheck) over flymake
   (setq lsp-enable-xref t)
-
-  ;; (setq netrom--general-lsp-hydra-heads
-  ;;       '(;; Xref
-  ;;         ("d" xref-find-definitions "Definitions" :column "Xref")
-  ;;         ("D" xref-find-definitions-other-window "-> other win")
-  ;;         ("r" xref-find-references "References")
-  ;;         ("s" netrom/helm-lsp-workspace-symbol-at-point "Helm search")
-  ;;         ("S" netrom/helm-lsp-global-workspace-symbol-at-point "Helm global search")
-  ;;         ("a" netrom/xref-find-apropos-at-point "Apropos")
-
-  ;;         ;; Peek
-  ;;         ("C-d" lsp-ui-peek-find-definitions "Definitions" :column "Peek")
-  ;;         ("C-r" lsp-ui-peek-find-references "References")
-  ;;         ("C-i" lsp-ui-peek-find-implementation "Implementation")
-
-  ;;         ;; LSP
-  ;;         ("p" lsp-describe-thing-at-point "Describe at point" :column "LSP")
-  ;;         ("C-a" lsp-execute-code-action "Execute code action")
-  ;;         ("R" lsp-rename "Rename")
-  ;;         ("t" lsp-goto-type-definition "Type definition")
-  ;;         ("i" lsp-goto-implementation "Implementation")
-  ;;         ("f" helm-imenu "Filter funcs/classes (Helm)")
-  ;;         ("C-c" lsp-describe-session "Describe session")
-
-  ;;         ;; Flycheck
-  ;;         ("l" lsp-ui-flycheck-list "List errs/warns/notes" :column "Flycheck"))
-
-  ;;       netrom--misc-lsp-hydra-heads
-  ;;       '(;; Misc
-  ;;         ("q" nil "Cancel" :column "Misc")
-  ;;         ("b" pop-tag-mark "Back")))
-
-  ;; ;; Create general hydra.
-  ;; (eval `(defhydra netrom/lsp-hydra (:color blue :hint nil)
-  ;;          ,@(append
-  ;;             netrom--general-lsp-hydra-heads
-  ;;             netrom--misc-lsp-hydra-heads)))
-
-  ;; (add-hook 'lsp-mode-hook
-  ;;           (lambda () (local-set-key (kbd "C-c C-l") 'netrom/lsp-hydra/body)))
   )
 
 
@@ -131,9 +58,13 @@
         lsp-ui-peek-peek-height 25)
   (add-hook 'lsp-mode-hook 'lsp-ui-mode))
 
+
+
 (use-package company
   :config
   (global-set-key (kbd "C-/") 'company-complete))
+
+
 
 (use-package company-lsp
   :requires company
@@ -145,46 +76,3 @@
   (setq company-transformers nil
         company-lsp-async t
         company-lsp-cache-candidates nil))
-
-(use-package company-box
-  :hook (company-mode . company-box-mode)
-  :config
-
-  (setq company-box-icons-unknown 'fa_question_circle)
-
-  (setq company-box-icons-elisp
-        '((fa_tag :face font-lock-function-name-face) ;; Function
-          (fa_cog :face font-lock-variable-name-face) ;; Variable
-          (fa_cube :face font-lock-constant-face) ;; Feature
-          (md_color_lens :face font-lock-doc-face))) ;; Face
-
-  (setq company-box-icons-yasnippet 'fa_bookmark)
-
-  (setq company-box-icons-lsp
-        '((1 . fa_text_height) ;; Text
-          (2 . (fa_tags :face font-lock-function-name-face)) ;; Method
-          (3 . (fa_tag :face font-lock-function-name-face)) ;; Function
-          (4 . (fa_tag :face font-lock-function-name-face)) ;; Constructor
-          (5 . (fa_cog :foreground "#FF9800")) ;; Field
-          (6 . (fa_cog :foreground "#FF9800")) ;; Variable
-          (7 . (fa_cube :foreground "#7C4DFF")) ;; Class
-          (8 . (fa_cube :foreground "#7C4DFF")) ;; Interface
-          (9 . (fa_cube :foreground "#7C4DFF")) ;; Module
-          (10 . (fa_cog :foreground "#FF9800")) ;; Property
-          (11 . md_settings_system_daydream) ;; Unit
-          (12 . (fa_cog :foreground "#FF9800")) ;; Value
-          (13 . (md_storage :face font-lock-type-face)) ;; Enum
-          (14 . (md_closed_caption :foreground "#009688")) ;; Keyword
-          (15 . md_closed_caption) ;; Snippet
-          (16 . (md_color_lens :face font-lock-doc-face)) ;; Color
-          (17 . fa_file_text_o) ;; File
-          (18 . md_refresh) ;; Reference
-          (19 . fa_folder_open) ;; Folder
-          (20 . (md_closed_caption :foreground "#009688")) ;; EnumMember
-          (21 . (fa_square :face font-lock-constant-face)) ;; Constant
-          (22 . (fa_cube :face font-lock-type-face)) ;; Struct
-          (23 . fa_calendar) ;; Event
-          (24 . fa_square_o) ;; Operator
-          (25 . fa_arrows)) ;; TypeParameter
-        )
-  )
